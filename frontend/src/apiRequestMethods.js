551ab4805c6bd9985454b309e7e035f384b53d8f
masterghost2002 <rakeshdhariwal61@gmail.com>
2023-04-30 20:20:24 +0530

    profile section beta

--- frontend/src/apiRequestMethods.js
+++ frontend/src/apiRequestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const BASE_URL = "http://localhost:5000/api";
-// export const BASE_URL = 'https://meowchat-backend-production.up.railway.app/api';
+// export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = 'https://meowchat-backend-production.up.railway.app/api';
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
     withCredentials:true
